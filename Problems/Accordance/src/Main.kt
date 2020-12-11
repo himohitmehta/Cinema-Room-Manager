5baf8c0888b8a39828b72f1162696f6cd09bfb7c
thenameismohit <mehtam2701@gmail.com>
2020-12-11 21:41:36 +0530

    Stage 4/5 Completed

---
+++ Problems/Accordance/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    // put your code here
+    val array2d = arrayOf(
+            arrayOf("[0][0]", "[0][1]", "[0][2]"),
+            arrayOf("[1][0]", "[1][1]", "[1][2]")
+    )
+    println(array2d.contentDeepToString())
+}
